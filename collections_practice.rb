b7d2362c8baa164809490c457307b18fcba87b4c
Dennis <dennis.basyrov@gmail.com>
2019-05-28 03:39:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -47,14 +47,14 @@ def count_elements(array)
 end
 
 def merge_data(arr1, arr2)
-  arr2[0].map do |name, hash|
-    new_hash = {}
-    arr1.each do |new_hash|
+  arr2[0].map do |name, prop_hash|
+    new_prop_hash = {}
+    arr1.each do |new_attr_hash|
       if new_attr_hash[:first_name] == name
-        new_hash = prop_hash.merge(new_attr_hash)
+        new_prop_hash = prop_hash.merge(new_attr_hash)
       end
     end
-    new_hash
+    new_prop_hash
   end
 end
 
