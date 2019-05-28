840522533183c53fbeff08c6e3204965abe089b9
Dennis <dennis.basyrov@gmail.com>
2019-05-28 03:38:17 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -45,3 +45,16 @@ def count_elements(array)
     end
   end.uniq
 end
+
+def merge_data(arr1, arr2)
+  arr2[0].map do |name, hash|
+    new_hash = {}
+    arr1.each do |new_attr_hash|
+      if new_attr_hash[:first_name] == name
+        new_hash = prop_hash.merge(new_attr_hash)
+      end
+    end
+    new_hash
+  end
+end
+
